16ca8004d999d3c912bdb0e0322a8505d70c633e
Nikita Kravchenko <nickravchenko98@gmail.com>
2023-03-01 08:59:26 -0400

    Move helper functions to root folder

--- links_extractors/uni_links_extractor.py
+++ links_extractors/uni_links_extractor.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import io, json
-from helper_functions import readFile, writeFile
+from ..helper_functions import readFile, writeFile
 
 
 universities_links_wiki = readFile("data/universities_links.json")
---
+++ pages_scrapper/scrapper.py
@@ -0,0 +1,4 @@
+import requests
+from bs4 import BeautifulSoup
+import io, json
+from ..helper_functions import readFile, writeFile

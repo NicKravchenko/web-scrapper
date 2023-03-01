88349224725cc7913a82222b328bb264d34e9b5e
Nikita Kravchenko <nickravchenko98@gmail.com>
2023-03-01 17:45:11 -0400

    Page extractor working

--- pages_scrapper/scrapper.py
+++ pages_scrapper/scrapper.py
@@ -18,6 +18,67 @@ headers = {
 }
 
 uni_links = readFile("data/universities_world.json")
+unis_all_links = readFile("data/unis_all_links.json")
+
+
+def get_links(base_url, detail_url, session, unis_all_links):
+    """Get rid of http or https"""
+    url = base_url + detail_url
+    url_no_http = return_clean_link(url)
+    response = session.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        for a in soup.find_all("a"):
+            href = str(a.get("href"))
+            """Add href only if its relative path or it contains name of page"""
+            if href[:1] == "/" or (url_no_http in href):
+                composed_url = base_url + href
+                if composed_url not in unis_all_links[base_url]:
+                    print(composed_url)
+
+                    unis_all_links[base_url].append(composed_url)
+
+                    writeFile("data/unis_all_links.json", unis_all_links)
+
+                    get_links(base_url, href, session, unis_all_links)
+
+
+def decompose_page(url, session):
+    hList = {}
+    page_text = ""
+
+    """Get rid of http or https"""
+    url_no_http = return_clean_link(url)
+
+    response = session.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        text = cleanify_soup_text(soup)
+
+        for a in soup.find_all("a"):
+            href = str(a.get("href"))
+            """Add href only if its relative path or it contains name of page"""
+            if href[:1] == "/":  # or (url_no_http in href):
+                links.append(href)
+
+        for i in range(1, 7):
+            hList[i] = []
+            for h in soup.find_all(f"h{i}"):
+                hList[i].append(h.text)
+
+        intec_json[url] = {}
+        intec_json[url]["header"] = hList
+
+        intec_json[url]["body"] = text
+
+        print(text)
+        print(intec_json)
+        print(links)
+
 
 try:
     """Start session for retrieving one domain"""
@@ -31,61 +92,18 @@ try:
     session.verify = cert_path
 
     for uni in uni_links:
-        # url = uni_links[uni]
+        # base_url = uni_links[uni]
         # print(uni)
         # url = "https://github.com/4teamwork/ftw.linkchecker/issues/57"
-        # url = "http://www.intec.edu.do/"
-        url = "http://www.au.edu.az/"
+        # base_url = "http://www.intec.edu.do"
+        base_url = "http://www.au.edu.az/"
+        unis_all_links[base_url] = []
 
-        links = []
-        hList = {}
-        page_text = ""
-
-        """Get rid of http or https"""
-        url_no_http = return_clean_link(url)
-
-        response = session.get(url)
-
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, "html.parser")
-
-            text = cleanify_soup_text(soup)
-
-            for a in soup.find_all("a"):
-                href = str(a.get("href"))
-                """Add href only if its relative path or it contains name of page"""
-                if href[:1] == "/" or (url_no_http in href):
-                    links.append(href)
-
-            for i in range(1, 7):
-                hList[i] = []
-                for h in soup.find_all(f"h{i}"):
-                    hList[i].append(h.text)
-
-            intec_json[url] = {}
-            intec_json[url]["header"] = hList
-
-            intec_json[url]["body"] = text
-
-            # print(text)
-            print(intec_json)
-            # print(links)
-
-            writeFile("data/intec.json", intec_json)
+        get_links(base_url, "", session, unis_all_links)
+        writeFile("data/unis_all_links.json", unis_all_links)
 
         break
 
 
 except Exception as e:
     print(e)
-
-
-# writeFile("data/intec.json", intec_json)
-
-
-# response = requests.get(
-#     url,
-#     # verify=False
-#     verify=cert_path,
-#     headers=headers,
-# )

a1afb241a66c6021345466c661f93ff6235046b3
Nikita Kravchenko <nickravchenko98@gmail.com>
2023-02-28 20:29:35 -0400

    Getting links of universities by wiki link

--- page_extractor.py
+++ page_extractor.py
@@ -9,32 +9,46 @@ base_url = "https://en.wikipedia.org"
 universities_links = readFile("data/universities_links.json")
 universities = readFile("data/universities.json")
 
-for key in universities:
-    detail_url = universities[key]
-    universitiesOfCountry = {}
+try:
+    for key in universities:
+        detail_url = universities[key]
+        universitiesOfCountry = {}
 
-    url = detail_url
-    print(url)
-    response = requests.get(url)
+        url = detail_url
+        print(url)
+        response = requests.get(url)
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "html.parser")
 
-        for link in soup.find_all("a"):
-            href = str(link.get("href"))
-            title = str(link.get("title"))
+            for link in soup.find_all("a"):
+                href = str(link.get("href"))
+                title = str(link.get("title"))
+                # "university"
+                # or "universidad"
+                # or "college" instituto
 
-            if ("university" or "universidad") in href.lower():
-                universitiesOfCountry[title] = base_url + href
-                print(title + " - " + href)
+                if (
+                    "university"
+                    or "institute"
+                    or "universidad"
+                    or "college"
+                    or "instituto"
+                ) in href.lower():
+                    universitiesOfCountry[title] = base_url + href
+                    print(title + " - " + href)
 
-            # print(href)
-        universities_links[key] = universitiesOfCountry
-        # print(soup.prettify())
-        # break
+                # print(href)
+            universities_links[key] = universitiesOfCountry
+            # print(soup.prettify())
+            # break
 
-    else:
-        print("Error: Could not fetch content")
+        else:
+            print("Error: Could not fetch content")
+except Exception as e:
+    writeFile("data/universities_links.json", universities_links)
+    print(e)
 
 writeFile("data/universities_links.json", universities_links)
+
 # print(universities_links)

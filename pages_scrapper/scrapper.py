fca497b6c594e5c8c4022cb9bff6ee3895ba56a6
Nikita Kravchenko <nickravchenko98@gmail.com>
2023-03-02 12:40:53 -0400

    Add retries

--- pages_scrapper/scrapper.py
+++ pages_scrapper/scrapper.py
@@ -65,7 +65,7 @@ def get_links(base_url, detail_url, session, unis_all_links, recursion_depth):
         if ("http://" in href) or ("https://" in href):
             composed_url = href
 
-        response_check_if_404 = session.get(composed_url, timeout=10)
+        response_check_if_404 = session.get(composed_url, timeout=20)
 
         if (composed_url in unis_all_links[base_url]) or (
             not response_check_if_404
@@ -145,9 +145,12 @@ try:
     )
     session.verify = cert_path
 
+    session.adapters.DEFAULT_RETRIES = 3
+
     for uni in uni_links:
         # base_url = uni_links[uni]
         base_url = "https://www.intec.edu.do"
+        # base_url = "https://aab-edu.net/"
         if base_url not in unis_all_links:
             unis_all_links[base_url] = []
 
